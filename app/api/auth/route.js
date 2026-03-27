e832b53adf0d146d9500a6ad3f0a435d19bd7d21
JulienR <raynaudjulienpro@gmail.com>
2026-03-27 11:34:21 +0100

    test bdd

--- app/api/auth/route.js
+++ app/api/auth/route.js
@@ -36,7 +36,7 @@ export async function POST(request) {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'strict',
-      maxAge: 60 * 60 * 24 * 7, // 7 jours
+      maxAge: 60 * 60 * 24 * 7,
       path: '/',
     })
 
