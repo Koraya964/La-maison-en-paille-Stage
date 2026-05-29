32c9bf8b7185e45ec8dac93db824da193afbc00f
JulienR <raynaudjulienpro@gmail.com>
2026-05-29 15:58:26 +0200

    Add partitioned option to cookie settings

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -4,6 +4,7 @@ import { signToken } from '../lib/auth.js';
 const COOKIE_OPTIONS = {
     httpOnly: true,
     secure: true,
+    partitioned: true,
     sameSite: 'none',
     path: '/',
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 jours en ms
